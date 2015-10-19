fa022d82e0f05579e82135f55dbd8720b85c92e8
Jefferson Gouveia <jeffersongouveia1@hotmail.com>
2015-10-19 15:23:12 -0400

    Mais alguns ajustes no Fullcalendar

--- resources/views/agendamentos/index.blade.php
+++ resources/views/agendamentos/index.blade.php
@@ -11,30 +11,46 @@
 
     <script>
         $(document).ready(function() {
-            $('#calendar').fullCalendar({
+            var calendar = $('#calendar');
+            calendar.fullCalendar({
                 header: {
                     left: 'prev,next today',
                     center: 'title',
                     right: 'month,agendaWeek,agendaDay'
                 },
-                defaultView: 'month',
+                defaultView: 'agendaWeek',
+                businessHours: {
+                    start: '07:00',
+                    end: '23:00',
+                    dow: [1, 2, 3, 4, 5, 6]
+                },
+                minTime: '07:00',
+                maxTime: '22:30',
                 editable: true,
-                //hiddenDays: [0], oculta o Domingo
+                //hiddenDays: [0], //oculta o Domingo
                 aspectRatio: 3.25,
 
-                dayClick: function(date, jsEvent, view) {
-                    var moment = $('#calendar').fullCalendar('getDate');
+                dayClick: function(date) { //removi "allDay, jsEvent, view" da função
+                    var moment = calendar.fullCalendar('getDate');
 
                     //o usuario nao precisa reservar a sala para o passado ;)
                     if (date.format() < moment.format('YYYY-MM-DD')) {
                         alert('Você não pode reservar neste dia!');
                     }
                     else {
-                        //passa para o form a data e a hora
-                        $('#myModalLabel').text('Data: ' + date.format('DD/MM/YYYY'));
-                        $('#horaId').text(date.format('HH:MM'));
+                        var view = calendar.fullCalendar('getView');
 
-                        $('#createModal').modal('show');
+                        //verifica se o calendario está no modo mês
+                        if (view.name == 'month') {
+                            calendar.fullCalendar('changeView', 'agendaDay');
+                            calendar.fullCalendar('gotoDate', date);
+                        } else {
+                            //passa para o form a data e a hora
+                            $('#myModalLabel').text('Data: ' + date.format('DD/MM/YYYY'));
+                            $('#horaId').text(date.format('HH:mm'));
+
+                            $('#createModal').modal('show');
+                        }
                     }
                 }
             });
